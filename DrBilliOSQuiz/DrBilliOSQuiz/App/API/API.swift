651338a29407e52bd053031ce89e3907207b1420
Shohe Ohtani <canada.shohe@gmail.com>
2016-08-18 16:32:13 -0700

    edit API to be able to (cancel / resume / suspend).

--- DrBilliOSQuiz/DrBilliOSQuiz/App/API/API.swift
+++ DrBilliOSQuiz/DrBilliOSQuiz/App/API/API.swift
@@ -15,27 +15,41 @@ import SwiftyJSON
 
 final class API {
     static let sharedInstance: API = API()
+    private var configuration: NSURLSessionConfiguration!
+    private var manager: Alamofire.Manager!
+    private var request: Alamofire.Request?
     
     private init() {
-        
+        configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        manager = Alamofire.Manager(configuration: configuration)
     }
     
-    static func getMusicRequest(key: String, callback: (JSON?) -> Void) {
-        Alamofire.request(.GET, "https://itunes.apple.com/search?term=\(key)&media=music&entity=song&country=us&lang=en_us&limit=10").responseJSON { response in
-            guard let object = response.result.value else {
-                return
+    static func getMusicRequest(key: String, callback: (JSON) -> Void) {
+        API.sharedInstance.manager.request(.GET, "https://itunes.apple.com/search?term=\(key)&media=music&entity=song&country=us&lang=en_us&limit=10", parameters: nil).responseJSON { (response) in
+            if let object = response.result.value {
+                callback(JSON(object))
             }
-            callback(JSON(object))
         }
     }
     
-    static func getBookRequest(key: String, callback: (JSON?) -> Void) {
-        Alamofire.request(.GET, "https://www.googleapis.com/books/v1/volumes?q=\(key)&country=US").responseJSON { response in
-            guard let object = response.result.value else {
-                return
+    static func getBookRequest(key: String, callback: (JSON) -> Void) {
+        API.sharedInstance.manager.request(.GET, "https://www.googleapis.com/books/v1/volumes?q=\(key)&country=US", parameters: nil).responseJSON { (response) in
+            if let object = response.result.value {
+                callback(JSON(object))
             }
-            callback(JSON(object))
         }
     }
     
+    static func resume() {
+        API.sharedInstance.request?.resume()
+    }
+    
+    static func suspend() {
+        API.sharedInstance.request?.suspend()
+    }
+    
+    static func cancel() {
+        API.sharedInstance.request?.cancel()
+    }
+
 }
