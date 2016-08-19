e0b1178fff80f405e1181f241d2ee304c29afc08
Shohe Ohtani <canada.shohe@gmail.com>
2016-08-19 03:03:59 -0700

    for keyboard method

--- DrBilliOSQuiz/DrBilliOSQuiz/App/ViewControllers/SearchViewController.swift
+++ DrBilliOSQuiz/DrBilliOSQuiz/App/ViewControllers/SearchViewController.swift
@@ -12,6 +12,7 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var _tableView: UITableView!
     @IBOutlet weak var _searchBar: UISearchBar!
+    @IBOutlet weak var tableViewBottomConstraint: NSLayoutConstraint!
     
     private let logic: SearchViewLogic = SearchViewLogic()
     
@@ -23,12 +24,58 @@ class SearchViewController: UIViewController {
         logic.viewDidLoad()
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        addKeyboardHandle()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    deinit {
+        removeKeyboardHandle()
+    }
+    
 }
+
+
+// MARK: - keyboard handling method.
+extension SearchViewController {
+    
+    func addKeyboardHandle() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: #selector(handleKeyboardWillShowNotification(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(handleKeyboardWillHideNotification(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyboardHandle() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        notificationCenter.removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func handleKeyboardWillShowNotification(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        tableViewBottomConstraint.constant = keyboardScreenEndFrame.size.height
+        commitLayoutAnimation()
+    }
+    
+    func handleKeyboardWillHideNotification(notification: NSNotification) {
+        tableViewBottomConstraint.constant = 0
+        commitLayoutAnimation()
+    }
+    
+    func commitLayoutAnimation() {
+        view.setNeedsUpdateConstraints()
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+    
+}
+
 
 // MARK: - SearchView
 extension SearchViewController: SearchView {
